cf3af4097305aec4f8da715fd7f2a377b0581258
Ashish Singh <ashishsingh4u@gmail.com>
2018-11-18 23:55:37 +0800

    Changes made to adapt new angular version

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import * as Rx from 'rxjs';
 
 @Injectable()
 
 export class DataService {
 
-  private goals = new BehaviorSubject<any>(['The initial goal', 'Another silly life goal']);
+  private goals = new Rx.BehaviorSubject<any>(['The initial goal', 'Another silly life goal']);
   goal = this.goals.asObservable();
 
   constructor() { }
 
   changeGoal(goal) {
-    this.goals.next(goal)
+    this.goals.next(goal);
   }
 }
